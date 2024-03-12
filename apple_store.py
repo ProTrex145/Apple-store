17ecc8bf1c235d5ce5209697afb7e979883ba242
ProTrex145 <adrianguerra853@gmail.com>
2024-03-12 09:26:32 -0500

    second commit

--- apple_store.py
+++ apple_store.py
@@ -3,17 +3,14 @@ print("***  Hello and welcome to  ***")
 print("***    the apple store     ***")
 print("******************************")
 print()
-print("What is your first name: ")
-first_name = input()
-print("What is your last name: ")
-last_name = input()
+first_name = input("What is your first name: ")
+last_name = input("What is your last name: ")
 full_name = first_name + " " + last_name
 apples = 20
 print()
 print("Welcome", full_name, "it is a pleasure to have you at our store today.")
 print("Righ now we have", apples, "apples each for $5.\n")
-print('How many would you like to buy?')
-amount = int(input())
+amount = int(input('How many would you like to buy? '))
 price = amount * 5
 apples = apples - amount
 print("\nYou bought:", amount," apples\nThe total price would be:", price," dollars\n")
